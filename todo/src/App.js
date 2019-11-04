eba40ccd1c86c6822a395bdf1cc0c9e218a65044
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2019-11-04 20:29:33 +0300

    touch now.json and modified app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const  someVariable = 'Hello!';
+const logics = true;
+const ternaryOperator = false;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+
+        {/* добавлениe в разметку объекта style; */}
+        <p style ={{
+            color: 'lightblue',
+            
+            // добавление числа:
+            fontSize: 45
+          }}>
           Hello, World!
+        </p>
+
+        {/* вывод значения переменной {}; */}
+        <p
+        style ={{
+          // добавление арифметической операции:
+          fontSize: 20+14,
+        }}>
+          {someVariable}
+        </p>
+        
+        {/* добавление результата логической операции */}
+        <p>
+          {logics && 'Logics is true'}
+        </p>
+
+        {/* добавление результата тернарного условия; */}
+        <p>
+          {ternaryOperator ? 'Ternary is true' : 'Ternary is false'}
+        </p>
+
+        {/* добавление объектов: */}
+        <p>
+          {null}
+          {undefined}
+          {false}
+          {true}
         </p>
         <a
           className="App-link"
