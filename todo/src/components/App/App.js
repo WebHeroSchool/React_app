5e1932debba669222c114226b8c15229ca39746b
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2019-11-08 17:26:41 +0300

    Задание #22: Подключение стилей

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList.js';
 import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
+import './App.css';
+
 const todoItem = [
   {value: 'Сходить на рынок'},
   {value: 'Купить котиков, которые стреляют лазером из глаз'},
   {value: 'Захватить мир'}
 ];
 
-const App = () => (<div>
+const App = () => (<div className = 'wrap'>
   <h1>Важные дела:</h1>
   <InputItem />
   <ItemList todoItem={ todoItem }/>
