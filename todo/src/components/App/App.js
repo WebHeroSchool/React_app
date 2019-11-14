6a58b4d8a560101f0f69b88792c6f9474d7dd26f
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-11-14 20:52:50 +0300

    Work #23: CSS Modules

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList.js';
 import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
-import './App.css';
+import styles from './App.module.css';
 
 const todoItem = [
   {value: 'Сходить на рынок'},
@@ -10,8 +10,8 @@ const todoItem = [
   {value: 'Захватить мир'}
 ];
 
-const App = () => (<div className = 'wrap'>
-  <h1>Важные дела:</h1>
+const App = () => (<div className = {styles.wrap}>
+  <h1 className = {styles.title}>Важные дела:</h1>
   <InputItem />
   <ItemList todoItem={ todoItem }/>
   <Footer todoListAll={3}/>
